b79c8fe778ac595332831a6d1a2a457f4c3e307d
Abs <webdesign.banerji@gmail.com>
2022-02-02 13:21:45 -0500

    Commit with GitIgnore

--- ec2/admin.py
+++ ec2/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-#from ec2.models import Instance
+from .models import Instance
 # Register your models here.
 
+@admin.register(Instance)
+
 class InstanceAdmin(admin.ModelAdmin):
+    readonly_fields = ("instance_id", "instance_state", )
     pass
     
 #admin.site.register(Instance, InstanceAdmin)
